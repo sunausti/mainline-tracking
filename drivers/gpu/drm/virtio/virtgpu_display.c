2b1bef4fe8ff3c5374535d7aa5f22d91102af497
imititel <ionel-catalin.mititelu@intel.com>
2024-09-25 11:08:35 +0000

    drm/virtio: Properly handle return value of virtqueue_enable_cb
    Reported-by: imititel <ionel-catalin.mititelu@intel.com>
    Signed-off-by: Weifeng Liu <weifeng.liu@intel.com>

--- drivers/gpu/drm/virtio/virtgpu_display.c
+++ drivers/gpu/drm/virtio/virtgpu_display.c
@@ -54,9 +54,12 @@ static int virtio_irq_enable_vblank(struct drm_crtc *crtc)
 	struct drm_device *dev = crtc->dev;
 	struct virtio_gpu_device *vgdev = dev->dev_private;
 	struct virtio_gpu_output *output = drm_crtc_to_virtio_gpu_output(crtc);
+	int ret;
 
-	virtio_gpu_vblank_poll_arm(vgdev->vblank[output->index].vblank.vq);
-	virtqueue_enable_cb(vgdev->vblank[output->index].vblank.vq);
+	do {
+		virtio_gpu_vblank_poll_arm(vgdev->vblank[output->index].vblank.vq);
+	} while (!virtqueue_enable_cb(vgdev->vblank[output->index].vblank.vq));
+
 	return 0;
 }
 
